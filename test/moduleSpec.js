17ce198a12c8d79bfbd0cece132f2ebebc556341
Vitor Nascimento Araujo <vitornascimentoaraujo@gmail.com>
2017-06-30 01:05:18 -0300

    renomeia var moduleMath para moduleTest

--- test/moduleSpec.js
+++ test/moduleSpec.js
@@ -1,38 +1,38 @@
 var expect = require('chai').expect;
-var moduleMath = require('../src/module');
+var moduleTest = require('../src/module');
 
-describe('module', () => {
+describe('Stryker Tests [ module ]', () => {
 
     it('Soma dos dois números deve ser 15', () => {
         var numberOne = 10;
         var numberTwo = 5;
-        expect(moduleMath.sum(numberOne, numberTwo)).to.equal(15);
+        expect(moduleTest.sum(numberOne, numberTwo)).to.equal(15);
     });
 
     it('O valor do incremento + 1 de 5 deve ser 6', () => {
         var number = 5;
         var increment = 6;
-        expect(moduleMath.increment(number)).to.equal(increment);
+        expect(moduleTest.increment(number)).to.equal(increment);
     });
 
-    it('Deve decrementar o número', () => {
+    it('O valo rdo incremento -1 de 2 deve ser 1', () => {
         var number = 2;
         var decrement = 1;
-        expect(moduleMath.decrement(number)).to.equal(decrement);
+        expect(moduleTest.decrement(number)).to.equal(decrement);
     });
 
     it('Deve retornar verdadeiro para valor negativo', () => {
         var negativeNumber = -2;
-        expect(moduleMath.isNegativeNumber(negativeNumber)).to.be.true;
+        expect(moduleTest.isNegativeNumber(negativeNumber)).to.be.true;
     });
 
     xit('Deve retornar falso para valor zero', () => {
         var neutralValue = 0;
-        expect(moduleMath.isNegativeNumber(neutralValue)).to.be.false;
+        expect(moduleTest.isNegativeNumber(neutralValue)).to.be.false;
     });
 
     it('Deve retornar falso para valor positivo', () => {
         var positiveNumber = 4;
-        expect(moduleMath.isNegativeNumber(positiveNumber)).to.be.false;
+        expect(moduleTest.isNegativeNumber(positiveNumber)).to.be.false;
     });
 });
