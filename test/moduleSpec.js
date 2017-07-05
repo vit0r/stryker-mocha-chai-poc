1e6a6fc66e547a4a8c16eb81a8d08988c1cca4c2
Vitor Nascimento Araujo <vitornascimentoaraujo@gmail.com>
2017-07-04 22:15:07 -0300

    ajusta espaços entre blocos

--- test/moduleSpec.js
+++ test/moduleSpec.js
@@ -1,57 +1,46 @@
-describe('Stryker Tests [ module ]', () => {
+let expect = require('chai').expect
 
-    let expect = require('chai').expect,
-        moduleTest = require('../src/module');
+describe('Stryker Tests [ moduleTest ]', () => {
+    
+    let moduleTest = require('../src/module');
 
     it('Somar valores', () => {
-
         let numberOne = 10,
             numberTwo = 5,
             expectedSum = 15,
             sum = moduleTest.sum(numberOne, numberTwo);
-
         expect(sum).to.equal(expectedSum);
     });
 
     it('Incrementar valor +1', () => {
-
         let number = 5,
             expectedIncrement = 6,
             increment = moduleTest.increment(number);
-
         expect(increment).to.equal(expectedIncrement);
     });
 
     it('Decrementar valor -1', () => {
-
         let number = 2,
             expectedDecrement = 1,
             decrement = moduleTest.decrement(number);
-
         expect(decrement).to.equal(expectedDecrement);
     });
 
     it('Deve retornar verdadeiro para valor negativo', () => {
-
         let number = -2,
             isNegativeNumber = moduleTest.isNegativeNumber(number);
-
         expect(isNegativeNumber).to.be.true;
     });
 
     it('Deve retornar falso para valor positivo', () => {
-
         let number = 2,
             isNegativeNumber = moduleTest.isNegativeNumber(number);
-
         expect(isNegativeNumber).to.be.false;
     });
 
     xit('Deve retornar falso para valor zero', () => {
-
         let number = 0,
             isNegativeNumber = moduleTest.isNegativeNumber(number);
-
         expect(isNegativeNumber).to.be.false;
     });
 });
